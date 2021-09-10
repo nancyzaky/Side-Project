b39dfed6ffc5f70025b669eb7334988b405a8bad
David Zombar <davidzombar@gmail.com>
2021-09-10 11:18:07 -0600

    added Menu icon for Favorites in HeaderBar.js

--- side-project/src/App.js
+++ side-project/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import HeaderBar from './components/HeaderBar';
 
 function App() {
-  return <div>Hello, Team!</div>;
+  return (
+    <div>
+      <HeaderBar />
+    </div>
+  );
 }
 
 export default App;
--- side-project/src/components/HeaderBar.js
+++ side-project/src/components/HeaderBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 function HeaderBar() {
-  return <div>test</div>;
+  return (
+    <div className='Header_Bar'>
+      <button className='Favs-Menu'>
+        <AiOutlineMenu className='Menu-Icon' />
+      </button>
+    </div>
+  );
 }
 
 export default HeaderBar;
